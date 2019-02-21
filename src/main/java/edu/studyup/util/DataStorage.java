1ce1e33966dded5e68f233037dbc36645a207a63
Yara Fawaz <yarafawaz96@gmail.com>
2019-02-20 20:52:41 -0800

    hw 3 - actually fixed bugs in the right files this time

--- src/main/java/edu/studyup/util/DataStorage.java
+++ src/main/java/edu/studyup/util/DataStorage.java
@@ -14,5 +14,5 @@ import edu.studyup.entity.Event;
  * 
  */
 public class DataStorage {
-	public static Map<Integer, Event> eventData = new HashMap<Integer, Event>();
+	public final Map<Integer, Event> eventData = new HashMap<Integer, Event>();
 }
